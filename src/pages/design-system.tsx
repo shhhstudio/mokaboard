85b5c45f7dbda51ea5cfbfde874e20fb33186d21
Romain Cochet <cochet@gmail.com>
2025-06-09 10:11:49 +0100

    Simple Board

--- src/pages/design-system.tsx
+++ src/pages/design-system.tsx
@@ -1,11 +1,10 @@
 import * as React from "react";
-import { Center, SimpleGrid, Box, Heading } from "@chakra-ui/react";
+import { Center, SimpleGrid, Flex, Heading } from "@chakra-ui/react";
 import { StatusWidget, TitleWidget } from "@/components/widgets";
 
 const DesignSystemPage: React.FC = () => {
   return (
-    <Box minH="100vh" bg="white" p={0}>
-      <Heading mb={8} textAlign="center">MOKABOARD Design System</Heading>
+    <Flex minH="100vh" bg="white" p={0} alignItems={"center"} justifyContent="center">
       <SimpleGrid columns={[2, 2, 3, 4]} gap={[2, 6, 6, 8]} width="max-content" marginX="auto">
         <StatusWidget title="Prod Malaisie lancée. Prod Chine en finalisation." status="success" />
         <StatusWidget title="Pas de Master Plan. Ventes en RUN." status="warning" />
@@ -15,12 +14,8 @@ const DesignSystemPage: React.FC = () => {
         <StatusWidget title="Weekly Update" status="error" />
         <StatusWidget title="Prod Malaisie lancée. Prod Chine en finalisation." status="success" />
         <StatusWidget title="Pas de Master Plan. Ventes en RUN." status="warning" />
-        <StatusWidget title="Weekly Update" status="error" />
-        <StatusWidget title="Prod Malaisie lancée. Prod Chine en finalisation." status="success" />
-        <StatusWidget title="Pas de Master Plan. Ventes en RUN." status="warning" />
-        <StatusWidget title="Weekly Update" status="error" />
       </SimpleGrid>
-    </Box>
+    </Flex>
   );
 };
 
